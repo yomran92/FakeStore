295855f7f41133cb03d82cb17b2baeb3a15079fd
yOmranOrbitech2 <y.omran2@orbitech.jo>
2026-01-07 20:05:18 -0500

    readme

--- lib/core/configurations/app_configuration.dart
+++ lib/core/configurations/app_configuration.dart
@@ -3,7 +3,7 @@
 class AppConfigurations {
 
   static String BaseUrl = 'https://fakestoreapi.com/';
-   static int PageSize = 4;
+   static int PageSize = 5;
    static int ListLimit = 20;
 
   static const Map<String, String> BaseHeaders = {
